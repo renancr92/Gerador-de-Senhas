2b6b341739dca9abaa54e92155096ff036876370
Renan <renancardosodarocha@gmail.com>
2026-09-20 10:57:42 -0300

    teste incial gerador de senha

--- src/gerenciadorDeSenhas/Geradorsenha.java
+++ src/gerenciadorDeSenhas/Geradorsenha.java
@@ -2,4 +2,10 @@ package gerenciadorDeSenhas;
 
 public class Geradorsenha {
 
+	public static String gerarSenha() {
+		return "Senha123";
+	}
+	public static void main(String[] args) {
+		System.out.println(gerarSenha());
+	}
 }
