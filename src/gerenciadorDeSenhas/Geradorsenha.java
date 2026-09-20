edf7edc0066cf4e58ab0d160bbb2c6e328d7ed86
Renan <renancardosodarocha@gmail.com>
2026-09-20 11:34:36 -0300

    Adiciona geração de caractere aleatório

--- src/gerenciadorDeSenhas/Geradorsenha.java
+++ src/gerenciadorDeSenhas/Geradorsenha.java
@@ -1,11 +1,21 @@
 package gerenciadorDeSenhas;
 
+import java.util.Random;
+
 public class Geradorsenha {
 
-	public static String gerarSenha() {
-		return "Senha123";
-	}
-	public static void main(String[] args) {
-		System.out.println(gerarSenha());
-	}
+    public static String gerarSenha() {
+
+        String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        Random random = new Random();
+
+        int posicao = random.nextInt(letras.length());
+
+        return String.valueOf(letras.charAt(posicao));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(gerarSenha());
+    }
 }
