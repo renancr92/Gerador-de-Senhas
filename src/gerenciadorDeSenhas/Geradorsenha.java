8f59222a3eab41f3d608fe8d9d6ed8b202350d5a
Renan <renancardosodarocha@gmail.com>
2026-09-21 20:19:36 -0300

    Garante caracteres variados na senha

--- src/gerenciadorDeSenhas/Geradorsenha.java
+++ src/gerenciadorDeSenhas/Geradorsenha.java
@@ -10,13 +10,17 @@ public class Geradorsenha {
 	    String numeros = "0123456789";
 	    String especiais = "@#$%&*!";
 
-	    String caracteres = letras + numeros + especiais;
-
 	    Random random = new Random();
 
 	    String senha = "";
 
-	    for (int i = 0; i < tamanho; i++) {
+	    senha += letras.charAt(random.nextInt(letras.length()));
+	    senha += numeros.charAt(random.nextInt(numeros.length()));
+	    senha += especiais.charAt(random.nextInt(especiais.length()));
+
+	    String caracteres = letras + numeros + especiais;
+
+	    for (int i = 3; i < tamanho; i++) {
 	        int posicao = random.nextInt(caracteres.length());
 	        senha += caracteres.charAt(posicao);
 	    }
